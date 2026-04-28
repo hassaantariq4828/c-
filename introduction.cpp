1b7d1e1317a2c919ca750837f76ca99fc8876020
hassaantariq4828 <hassaantariq2132@gmail.com>
2026-04-28 09:59:14 +0500

    Introduction

---
+++ introduction.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"M Hassan Tariq Khan  I am a computer enginering student  International Islamic uinversity Islamabad. "<<endl;
+
+    return 0;
+
+}
